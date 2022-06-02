3a415804f715735b395cec93bb4a933213a280ae
fung11111111 <ifung9604@gmail.com>
2022-06-02 20:34:34 +0800

    add new question

---
+++ src/array/GroupAnagrams.java
@@ -0,0 +1,15 @@
+package array;
+
+import java.util.*;
+
+//leetcode 49
+//todo: add solution
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+
+
+        return res;
+    }
+
+}
